b62970a76d8313cc8eca3c2a8adc0d1cb87446e4
Matthew Diana <matthew.a.diana@gmail.com>
2016-12-26 16:37:52 -0500

    Add API example page

---
+++ src/main/resources/public/js/vanity.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#checkButton').click(function() {
+        $('#checkButton').prop('disabled', true).html('<i class="fa fa-refresh fa-spin fa-fw"></i> Checking...');
+        $('#checkResult').empty();
+        $.get('/check?plateNum=' + $('#licensePlateNum').val()).done(function(license) {
+            if (license.available) {
+                $('<div class="alert alert-success" role="alert">This license plate is available!</div>')
+                    .appendTo('#checkResult');
+            } else {
+                $('<div class="alert alert-danger" role="alert">This license plate is not available.</div>')
+                    .appendTo('#checkResult');
+            }
+            $('#checkButton').prop('disabled', false).html('Check');
+        });
+    });
+});
+
